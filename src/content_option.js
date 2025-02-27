d7f2d46fe8d97d85e4f147850b3f7db638bca59d
SathvikAyyasamy <71762233044@cit.edu.in>
2025-02-27 19:32:12 +0530

    Portfolio website created

--- src/content_option.js
+++ src/content_option.js
@@ -1,162 +1,181 @@
-const logotext = "JOHN";
+const logotext = "SATHVIK AYYASAMY";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Sathvik Ayyasamy",
+    description: "I’m Sathvik Web developer and Data Analyst,currently pursuing Masters in Decision and Computing Sciences at Coimbatore Institute of Technology, Coimbatore.",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Sathvik Ayyasamy",  
     animated: {
         first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        second: "I code good websites",
+        third: "I have a good knowledge in Data Analysis",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "I’m Sathvik Web developer and Data Analyst,currently pursuing Masters in Decision and Computing Sciences at Coimbatore Institute of Technology, Coimbatore.",
+    your_img_url: "https://i.imgur.com/hTmVJu6.png",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "About Myself",
+    aboutme: "Aspiring web developer with a solid background in computer vision and data analysis, driven to use technology for real world solutions.",
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+const certificates = [
+    {
+        title: "HTML5 / CSS3",
+        provider: "Udemy",
+        year: "2023",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        title: "Python and Django",
+        provider: "Udemy",
+        year: "2024",
     },
     {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
+        title: "Mastering JavaScript & jQuery",
+        provider: "Udemy",
+        year: "2025",
     },
+
 ];
 
-const skills = [{
+const skills = [
+    {
         name: "Python",
         value: 90,
     },
     {
-        name: "Djano",
+        name: "JavaScript",
         value: 85,
     },
     {
-        name: "Javascript",
+        name: "C Programming",
         value: 80,
     },
     {
-        name: "React",
+        name: "HTML5",
+        value: 85,
+    },
+    {
+        name: "CSS3",
+        value: 80,
+    },
+    {
+        name: "React.js",
+        value: 75,
+    },
+    {
+        name: "Node.js",
+        value: 80,
+    },
+    {
+        name: "Django",
+        value: 85,
+    },
+    {
+        name: "MS Excel",
+        value: 85,
+    },
+    {
+        name: "Power BI",
+        value: 80,
+    },
+    {
+        name: "Git",
+        value: 85,
+    },
+    {
+        name: "GitHub",
         value: 60,
     },
     {
-        name: "Jquery",
-        value: 85,
+        name: "MySQL",
+        value: 80,
     },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+
+const services = [
+    {
+        title: "Web Development",
+        description: "Building responsive and dynamic websites using modern web technologies like HTML, CSS, JavaScript, and frameworks such as React.js and Node.js.",
     },
     {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Analytics",
+        description: "Performing data analysis using tools like Power BI, MS Excel, and Jupyter Notebook to extract insights and support decision-making.",
     },
     {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        title: "Data Structures",
+        description: "Implementing efficient algorithms and data structures to optimize performance and solve complex computational problems.",
     },
+    {
+        title: "Data Mining",
+        description: "Extracting meaningful patterns from large datasets using statistical and machine learning techniques.",
+    },
+    {
+        title: "Machine Learning",
+        description: "Developing predictive models and AI solutions using Python libraries such as Scikit-Learn, TensorFlow, and PyTorch.",
+    },
+    {
+        title: "DBMS",
+        description: "Designing and managing databases using MySQL and MongoDB, ensuring efficient data storage and retrieval.",
+    }
 ];
+
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://i.imgur.com/VUC8283.jpeg",
+        description: "Speech Emotion Recognition System",
+        link: "https://github.com/Sathvik870/Speech_emotion_Recognition",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://i.imgur.com/08LFk6s.jpeg",
+        description: "Social Media App",
+        link: "https://github.com/Sathvik870/Social_media_app",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://i.imgur.com/ihWb7HR.png",
+        description: "Bus Pass Management System",
+        link: "https://github.com/Sathvik870/App_for_buspass",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://i.imgur.com/upyKI9J.jpeg",
+        description: "Car Recommendation System",
+        link: "https://github.com/Sathvik870/Car_Recommendation_System",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://i.imgur.com/BuQzdN2.jpeg",
+        description: "User Management System",
+        link: "https://github.com/Sathvik870/User_Mgt_system_CRUD",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://i.imgur.com/pEvnQFs.png",
+        description: "Attendance Management System",
+        link: "https://github.com/Sathvik870/Attendace_management_System",
     },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "sathvikayyasamyworks@gmail.com",
+    YOUR_FONE: "+91 9500455058",
+    description: "I am available on almost all social media. You can message me, I will reply within 24 hours.",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    YOUR_SERVICE_ID: "service_o3av7mq",
+    YOUR_TEMPLATE_ID: "template_gr8ur5a",
+    YOUR_USER_ID: "WM2gEfV8XL3gHf1W5",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/Sathvik870",
+    facebook: "https://m.facebook.com/profile.php?id=100069756931913",
+    linkedin: "https://linkedin.com/in/sathvik-ayyasamy-441751272",
+    instagram: "https://www.instagram.com/sathvik.exe_/",
 };
 export {
     meta,
     dataabout,
     dataportfolio,
-    worktimeline,
+    certificates,
     skills,
     services,
     introdata,
